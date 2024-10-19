a7f29bde42a44d97b0f4b3488e1ade535bdbcc28
Dobatymo <Dobatymo@users.noreply.github.com>
2024-10-19 19:44:25 +0800

    fix ci
    update pre-commit
    update cibuildwheel
    pin runners
    update actions/download-artifact
    fix cmake version
    move info to pyproject

--- linux-build.sh
+++ linux-build.sh
@@ -3,7 +3,7 @@ yum -y install nasm devtoolset-11-gcc
 apk update
 apk add --upgrade gcc nasm
 python -m pip install -U pip wheel
-python -m pip install -U cmake
+python -m pip install -U 'cmake>=3.5,<4.0'
 mkdir libjpeg-turbo-build
 cd libjpeg-turbo-build
 cmake -G"Unix Makefiles" ../libjpeg-turbo
